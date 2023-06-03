e29f07d9e2b0323e5d171ad22a0de544666f15e3
kimeshan <kimeshan@gmail.com>
2023-06-03 22:37:08 +0100

    Improve mobile styling

--- app/page.tsx
+++ app/page.tsx
@@ -19,63 +19,67 @@ export default function Home() {
 
   return (
     <div className="flex flex-col items-center">
-      <h1 className="text-4xl lg:text-8xl md:text-6xl font-bold p-5 md:p-10 text-center text-gray-700">
+      <h1 className="text-4xl lg:text-8xl md:text-6xl font-bold p-3 md:p-10 text-center text-gray-700">
         Time Since
       </h1>
-      <h2 className="text-3xl lg:text-6xl md:text-4xl font-bold p-5 md:p-10 text-center text-red-600">
+      <h2 className="text-3xl lg:text-6xl md:text-4xl font-bold p-1 md:p-10 text-center text-red-600">
         Manchester United last won the league
       </h2>
 
-      <div className="stats shadow-2xl m-5 lg:m-10 stats-vertical lg:stats-horizontal  text-center">
-        <div className="stat w-80 md:w-100">
-          <div className="stat-value text-4xl md:text-7xl">
-            {countUpTime.years}
+      {countUpTime === defaultTimeElapsed ? (
+        <progress className="progress progress-success w-56"></progress>
+      ) : (
+        <div className="stats shadow-2xl m-2 lg:m-10 stats-vertical lg:stats-horizontal  text-center">
+          <div className="stat w-80 md:w-100 ">
+            <div className="stat-value text-3xl md:text-7xl">
+              {countUpTime.years}
+            </div>
+            <div className="stat-desc text-2xl md:text-4xl text-red-700 p-2">
+              {countUpTime.yearSuffix}
+            </div>
           </div>
-          <div className="stat-desc text-2xl md:text-4xl text-red-700 p-2">
-            {countUpTime.yearSuffix}
+          <div className="stat w-80 lg:w-1/6">
+            <div className="stat-value text-3xl md:text-7xl">
+              {countUpTime.months}
+            </div>
+            <div className="stat-desc text-2xl md:text-4xl text-red-600 p-2">
+              {countUpTime.monthSuffix}
+            </div>
+          </div>
+          <div className="stat w-80 lg:w-1/6">
+            <div className="stat-value text-3xl md:text-7xl">
+              {countUpTime.days}
+            </div>
+            <div className="stat-desc text-2xl md:text-4xl text-red-500 p-2">
+              {countUpTime.daySuffix}
+            </div>
+          </div>
+          <div className="stat w-80 lg:w-1/6">
+            <div className="stat-value text-3xl md:text-7xl">
+              {countUpTime.hours}
+            </div>
+            <div className="stat-desc text-2xl md:text-4xl text-red-400 p-2">
+              {countUpTime.hourSuffix}
+            </div>
+          </div>
+          <div className="stat w-80 lg:w-1/6">
+            <div className="stat-value text-3xl md:text-7xl">
+              {countUpTime.minutes}
+            </div>
+            <div className="stat-desc text-2xl md:text-4xl text-red-300 p-2">
+              {countUpTime.minuteSuffix}
+            </div>
+          </div>
+          <div className="stat w-80 lg:w-1/6">
+            <div className="stat-value text-3xl md:text-7xl">
+              {countUpTime.seconds}
+            </div>
+            <div className="stat-desc text-2xl md:text-4xl text-red-200 p-2">
+              seconds
+            </div>
           </div>
         </div>
-        <div className="stat w-80 lg:w-1/6">
-          <div className="stat-value text-4xl md:text-7xl">
-            {countUpTime.months}
-          </div>
-          <div className="stat-desc text-2xl md:text-4xl text-red-600 p-2">
-            {countUpTime.monthSuffix}
-          </div>
-        </div>
-        <div className="stat w-80 lg:w-1/6">
-          <div className="stat-value text-4xl md:text-7xl">
-            {countUpTime.days}
-          </div>
-          <div className="stat-desc text-2xl md:text-4xl text-red-500 p-2">
-            {countUpTime.daySuffix}
-          </div>
-        </div>
-        <div className="stat w-80 lg:w-1/6">
-          <div className="stat-value text-4xl md:text-7xl">
-            {countUpTime.hours}
-          </div>
-          <div className="stat-desc text-2xl md:text-4xl text-red-400 p-2">
-            {countUpTime.hourSuffix}
-          </div>
-        </div>
-        <div className="stat w-80 lg:w-1/6">
-          <div className="stat-value text-4xl md:text-7xl">
-            {countUpTime.minutes}
-          </div>
-          <div className="stat-desc text-2xl md:text-4xl text-red-300 p-2">
-            {countUpTime.minuteSuffix}
-          </div>
-        </div>
-        <div className="stat w-80 lg:w-1/6">
-          <div className="stat-value text-4xl md:text-7xl">
-            {countUpTime.seconds}
-          </div>
-          <div className="stat-desc text-2xl md:text-4xl text-red-200 p-2">
-            seconds
-          </div>
-        </div>
-      </div>
+      )}
     </div>
   );
 }
